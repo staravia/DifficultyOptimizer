18c084e192edb22e83933121e229f392d73deb70
staravia <destaravia@gmail.com>
2020-04-07 17:27:20 -0700

    initial commit

---
+++ DifficultyOptimizer/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DifficultyOptimizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
